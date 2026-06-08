fff0893f132dc2fca65259cd7214d7e3b825f691
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-08 13:02:47 +0530

    added 2.py

---
+++ class1/2.py
@@ -0,0 +1,59 @@
+a = 10
+
+b = 10.5
+
+c = 3 + 4j
+
+d = "Hello"
+
+e = [1, 2, 3]
+
+f = (1, 2, 3)
+
+g = range(5)
+
+h = {"name": "Salaar"}
+
+i = {1, 2, 3}
+
+j = frozenset({1,2,3})
+
+k = True
+
+l = b"Hello"
+
+m = bytearray(5)
+
+n = memoryview(bytes(5))
+
+o = None
+
+print("int:", a)
+
+print("float:", b)
+
+print("complex:", c)
+
+print("str:", d)
+
+print("list:", e)
+
+print("tuple:", f)
+
+print("range:", list(g))
+
+print("dict:", h)
+
+print("set:", i)
+
+print("frozenset:", j)
+
+print("bool:", k)
+
+print("bytes:", l)
+
+print("bytearray:", m)
+
+print("memoryview:", n)
+
+print("NoneType:", o)
