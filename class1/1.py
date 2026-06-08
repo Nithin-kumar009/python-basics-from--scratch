d59e892fbb77a0cc38452d3d4927001f192d1b06
Nithin-kumar009 <vnithinkumar009@gmail.com>
2026-06-08 12:10:19 +0530

    added 1.py

---
+++ class1/1.py
@@ -0,0 +1,10 @@
+print(type(10))            # int
+print(type(10.5))          # float
+print(type(3 + 4j))        # complex
+print(type("Hello"))       # str
+print(type([1, 2, 3]))     # list
+print(type((1, 2, 3)))     # tuple
+print(type({1, 2, 3}))     # set
+print(type({"a": 1}))      # dict
+print(type(True))          # bool
+print(type(None))          # NoneType
